eac80a58a2b4fa4db45dd455bd8a48a020542d2c
cleanerro <romastepanyuk@gmail.com>
2022-01-10 02:43:02 +0200

    created getStatus and updateStatus

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,6 +19,14 @@ export const profileApi = {
     getProfile(userId: string) {
         return instance.get(`profile/${userId}`)
             .then(res => res.data)
+    },
+    getStatus(userId: string) {
+        return instance.get(`profile/status/${userId}`)
+            .then(res => res.data)
+    },
+    updateStatus(status: string) {
+        return instance.put(`profile/status`, {status})
+            .then(res => res.data)
     }
 }
 
